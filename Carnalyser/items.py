c6e75c8833600db8eae84803b32547989de1ce88
Jefferson  Fernando de Araujo <marmojezz@gmail.com>
2018-04-25 21:37:12 +0000

    Update items.py

--- Carnalyser/items.py
+++ Carnalyser/items.py
@@ -8,7 +8,20 @@
 import scrapy
 
 
-class CarnalyserItem(scrapy.Item):
+class Ads(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    brand = scrapy.Field()
+    model = scrapy.Field()
+    price = scrapy.Field()
+    year_manufactory = scrapy.Field()
+    year_model = scrapy.Field()
+    cylinder = scrapy.Field()
+    engine = scrapy.Field()
+    doors = scrapy.Field()
+    fuel = scrapy.Field()
+    km = scrapy.Field()
+    cambio = scrapy.Field()
+    desc = scrapy.Field()
+    
     pass
